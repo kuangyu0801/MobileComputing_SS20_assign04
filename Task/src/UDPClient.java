41a1f695047ff838927c11b1779899f102280cad
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-06-29 23:03:26 +0200

    feat: add basic UDP Server and client

---
+++ Task/src/UDPClient.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.*;
+
+public class UDPClient {
+    final static int PORT = 5008;
+    final static String TAG_RCV = "[RCV]";
+    final static String TAG_SEND = "[SEND]";
+    public static void main(String[] args) throws IOException {
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+
+        String sentence = "Hello, World!";
+        sendData = sentence.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, PORT);
+        clientSocket.send(sendPacket);
+        System.out.println(TAG_SEND + sentence);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+
+        String modifiedSentence = new String(receivePacket.getData());
+        System.out.println(TAG_SEND + modifiedSentence);
+        clientSocket.close();
+    }
+}
---
+++ Task/src/UDPServer.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class UDPServer {
+    final static int PORT = 5008;
+    final static String TAG_RCV = "[RCV]";
+    final static String TAG_SEND = "[SEND]";
+    public static void main(String[] args) throws IOException {
+        DatagramSocket serverSocket = new DatagramSocket(PORT);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+
+            String sentence = new String(receivePacket.getData());
+            System.out.println(TAG_RCV + sentence);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+            System.out.println(TAG_SEND + capitalizedSentence);
+        }
+    }
+}
